8d28066bc4f98acc235e89305a6884bf9682035e
ah2909 <113582946+ah2909@users.noreply.github.com>
2024-06-11 23:06:10 +0700

    Add shadcn component and config dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import { cn } from "@/lib/utils"
 
@@ -25,8 +26,15 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
           {children}
+        </ThemeProvider>
       </body>
     </html>
   );
