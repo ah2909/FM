8e06430ed5746ef19ad16e42ed010c419803f869
ah2909 <caoanhhuan2909@gmail.com>
2024-08-17 22:48:40 +0700

    Fix vercel deploy error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,5 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-const fetcher = (...args) => fetch(...args).then(res => res.json())
